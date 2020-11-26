0449175edf6483585a36ea5aca077461302b22f2
LilithRelish <laragoblin@gmail.com>
2020-11-26 13:01:39 +0100

    mas password

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 echo sha1("juan");
 
-$password = password_hash('admin1234', PASSWORD_DEFAULT);
-echo '<br>'.$password;
+$password1 = password_hash('ana', PASSWORD_DEFAULT);
+$password2 = password_hash('pepe', PASSWORD_DEFAULT);
+$password3 = password_hash('luis', PASSWORD_DEFAULT);
+echo '<br>'.$password1;
+echo '<br>'.$password2;
+echo '<br>'.$password3;
