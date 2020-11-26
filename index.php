34c0ac6c74fcdda4d65c09adfa5d0c9e282da1e7
LilithRelish <laragoblin@gmail.com>
2020-11-26 13:27:48 +0100

    passwords for everyone

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 echo sha1("juan");
 
 $password1 = password_hash('ana', PASSWORD_DEFAULT);
-$password2 = password_hash('pepe', PASSWORD_DEFAULT);
-$password3 = password_hash('luis', PASSWORD_DEFAULT);
+$password2 = password_hash('luis', PASSWORD_DEFAULT);
+$password3 = password_hash('jose', PASSWORD_DEFAULT);
+$password4 = password_hash('juan', PASSWORD_DEFAULT);
 echo '<br>'.$password1;
 echo '<br>'.$password2;
 echo '<br>'.$password3;
+echo '<br>'.$password4;
